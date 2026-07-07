156d9eda8e74b371f416ef76c4fbd4e30d9ba7a6
Fatna El bassraoui <elbassraouifatna@gmail.com>
2026-07-07 22:09:18 +0200

    fix: improve dark mode support in SearchBar component styling

--- src/app/components/ui/SearchBar.tsx
+++ src/app/components/ui/SearchBar.tsx
@@ -7,13 +7,13 @@ interface SearchBarProps {
 export const SearchBar: React.FC<SearchBarProps> = ({ value, setQuery }) => {
   return (
     <div className="max-w-md mx-auto bg-">
-      <div className="flex items-center border pl-4 gap-2 border-gray-500/30 h-[46px] rounded-full overflow-hidden max-w-md w-full">
+      <div className="flex items-center border pl-4 gap-2 border-gray-500/30 dark:border-zinc-500/30 dark:bg-zinc-600/40 h-[46px] rounded-full overflow-hidden max-w-md w-full">
         <input
           type="text"
           placeholder="Search"
           value={value}
           onChange={(e) => setQuery(e.target.value)}
-          className="w-full h-full outline-none text-gray-500 bg-transparent placeholder-gray-500 text-sm"
+          className="w-full h-full outline-none text-gray-500 dark:text-white bg-transparent placeholder:text-gray-500 text-sm"
         />
       </div>
     </div>
